f0e656d798058f97cc3eeb93eb35c766b4ff9119
Coder-Akanksha <shivi.aka1810@gmail.com>
2024-09-22 13:32:44 +0530

    While condition

--- if_else_Assignment_1.cpp
+++ if_else_Assignment_1.cpp
@@ -1,39 +1,48 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     cout << "*******Know your Grade by marks******";
-    int n;
-    cout << "\nPlease Enter you marks:";
-    cin >> n;
-    if (n < 0 || n > 100)
+    bool isCorectMarks = false; //flag to check condition
+    while (isCorectMarks != true)  //check if flag not true
     {
-        cout << "Please enter Marks between 0 to 100";
-    }
-    else if (n >= 90)
-    {
-        cout << "Your Grade is A";
-    }
-    else if (n >= 80 && n < 90)
-    {
-        cout << "Your Grade is B";
-    }
-     else if (n >= 70 && n < 80)
-    {
-        cout << "Your Grade is C";
-    }
-     else if (n >= 60 && n < 70)
-    {
-        cout << "Your Grade is D";
-    }
-    else if (n >= 50 && n < 60)
-    {
-        cout << "Your Grade is E";
-    }
-    
-    else
-    {
-        cout << "You are Failed";
+        cout << "\nPlease Enter you marks:";
+        int n;
+        cin >> n;
+        if (n < 0 || n > 100)
+        {
+            cout << "Please enter Marks between 0 to 100";
+        }
+        else if (n >= 90)
+        {
+            cout << "Your Grade is A";
+            isCorectMarks = true;
+        }
+        else if (n >= 80 && n < 90)
+        {
+            cout << "Your Grade is B";
+            isCorectMarks = true;
+        }
+        else if (n >= 70 && n < 80)
+        {
+            cout << "Your Grade is C";
+            isCorectMarks = true;
+        }
+        else if (n >= 60 && n < 70)
+        {
+            cout << "Your Grade is D";
+            isCorectMarks = true;
+        }
+        else if (n >= 50 && n < 60)
+        {
+            cout << "Your Grade is E";
+            isCorectMarks = true;
+        }
+
+        else
+        {
+            cout << "You are Failed";
+            isCorectMarks = true;
+        }
     }
 }
