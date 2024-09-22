f0eb319a77e6bd76b13134808bfe8220326ef9da
Coder-Akanksha <shivi.aka1810@gmail.com>
2024-09-22 12:39:07 +0530

    removed commented code

--- conditional.cpp
+++ conditional.cpp
@@ -1,20 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-   // int a;
-    //cin>>a;
-    //cout<<"hello"<< n <<endl;
-    // if a is +ve
-   /* if(a>0){
-        cout<<"A is positive"<<endl;
-    }
-    else{
-        cout<<"A is negative"<<endl;
-    }*/
-   int a,b;
-  // cin>> a>> b;
-  a=cin.get();
-   //cout<<"value of a and b is: "<<a <<" " <<b <<endl;
-   cout<<" value of a is :"<< a << endl;
-    
+int main()
+{
+    int a, b;
+    a = cin.get();
+    cout << " value of a is :" << a << endl;
 }
